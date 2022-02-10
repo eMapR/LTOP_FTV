005039ada2c46295ad7e1cbd26626986eea04bf0
brobertspierel <robertsb@oregonstate.edu>
2022-02-10 15:54:51 -0800

    ftv update

--- scripts/GEEjs/06lt_TransferFTV.js
+++ scripts/GEEjs/06lt_TransferFTV.js
@@ -32,18 +32,20 @@ var table = ee.FeatureCollection("users/ak_glaciers/LTOP_Laos_config_selected");
 //vertices image from LTOP
 var lt_vert = ee.Image("users/clarype/Optimized_LandTrendr_year_vert_array_ben").clip(geometry3);
 //image stack
-var servir_ic = ee.ImageCollection('projects/servir-mekong/regionalComposites').filterBounds(geometry3).select('swir1')
+var servir_ic = ee.ImageCollection('projects/servir-mekong/regionalComposites').filterBounds(geometry3).select('swir1'); 
 //it seems like there is an issue with the dates starting on January 1. This is likely the result of a time zone difference between where 
 //the composites were generated and what the LandTrendr fit algorithm expects from the timestamps. 
 servir_ic = servir_ic.map(function(img){
-  var date = img.get('system:time_start')
-  return img.set('system:time_start',ee.Date(date).advance(6,'month').millis())
+  var date = img.get('system:time_start'); 
+  return img.set('system:time_start',ee.Date(date).advance(6,'month').millis()); 
 }); 
 //this should be 2000 for the servir composites, test or change for another year if using a different collection 
 //get the start year automatically from the first composite
-var startYear = ee.Number.parse(servir_ic.first().get('system:index')).getInfo()
-var LTOP_band = 'yrs_vert_0'
-var min_obvs = ee.Number(lt_vert.bandNames().length()).getInfo()
+var startYear = ee.Number.parse(servir_ic.first().get('system:index')).getInfo(); 
+print('the start year is: ',startYear)
+var endYear = 2020; 
+var LTOP_band = 'yrs_vert_0'; 
+var min_obvs = ee.Number(lt_vert.bandNames().length()).getInfo(); 
 
 ////////////////////////////////////////////////////////////////////////////////////////////////////
 ////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -70,7 +72,7 @@ var mask = lt_vert.gte(startYear+1).toArray()
 
 //apply the mask to the array 
 breakpoints = breakpoints.arrayMask(mask); 
-Map.addLayer(breakpoints,{},'mask bps')
+// Map.addLayer(breakpoints,{},'mask bps')
 //add the constant image above as an array with the start year as the constant value
 breakpoints = breakpoints.arrayCat({
   image2:yearImg.toArray(),
@@ -79,15 +81,15 @@ breakpoints = breakpoints.arrayCat({
 
 //sort the array values so that the breakpoint years are in chronological order after adding a band
 breakpoints = breakpoints.arraySort(); 
-print(breakpoints,'breakpoints')
-Map.addLayer(breakpoints,{},'breakpoints')
+print(breakpoints,'breakpoints'); 
+Map.addLayer(breakpoints,{},'breakpoints'); 
 
 //breakpoints should now be ready to use as input to the LandTrendr fit algorithm 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 // here we need to insert the deal with the spikeThreshold masks. I think this will mean running the LT-fit algo three (or however many)
 // times there are distinct spikeThreshold values and then patching those images back together at the end. 
 
-var spike_vals = table.aggregate_array('spikeThreshold').distinct()
+var spike_vals = table.aggregate_array('spikeThreshold').distinct(); 
 //go through the distinct spikeThreshold vals, filter or mask the image 
 //to get each value and use that as a mask to get the areas of the constant image that we want 
 
@@ -99,9 +101,9 @@ var filter_pixels = spike_vals.map(function(x){
   //then we get a list of all the cluster_ids with that spike threshold value
   clusters = clusters.aggregate_array('cluster_id'); 
   //next we get all the pixels from the cluster image that match the cluster ids selected in the previous step (create a mask)
-  var mask = cluster_image.remap(clusters,ee.List.repeat(1,clusters.length()))
+  var mask = cluster_image.remap(clusters,ee.List.repeat(1,clusters.length())); 
   //add a prop with the spikeThreshold value so we can query it later
-  return mask.set('spikeThreshold',x)
+  return mask.set('spikeThreshold',x); 
 }); 
 
 //these are the masks we want for running LandTrendr fit 
@@ -123,7 +125,7 @@ var masked_ic = spike_masks.map(function(msk){
 
 var combined = masked_ic.mosaic(); 
 
-Map.addLayer(combined,{},'combined'); 
+//Map.addLayer(combined,{},'combined'); 
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 //implement a version of the disturbance detection that does not use the pre-cooked disturbance code from LT. 
@@ -134,20 +136,91 @@ Map.addLayer(combined,{},'combined');
 var ftv = combined.select(['swir1_fit']); 
 // Map.addLayer(ftv,{},'ftv only')
 
-var backwardsDifference = function(array) {
+var forwardDifference = function(array) {
   var left = array.arraySlice(0, 0, -1); 
   var right = array.arraySlice(0, 1); 
   //double check order
   return left.subtract(right); 
 }; 
 
+var backwardsDifference = function(array) {
+  var right = array.arraySlice(0, 0, -1); 
+  var left = array.arraySlice(0, 1); 
+  //double check order
+  return left.subtract(right); 
+}; 
+
+
 //these are the delta outputs but they can only have a duration of one year
-var difference = backwardsDifference(ftv); 
+var deltas = backwardsDifference(ftv); 
 
-Map.addLayer(difference,{},'diff'); 
+// Map.addLayer(deltas,{},'deltas'); 
 
-//now add something that can understand duration- this is the time since the last breakpoint? 
+//make shifted breakpoints which should be the length of the segments? 
+var segLengths = forwardDifference(breakpoints); 
+// Map.addLayer(segLengths,{},'shifted breakpoints')
 
+//make shifted delta outputs. Taking these differences should give the location of the breakpoints? 
+var yod = forwardDifference(deltas)//year of detection
+var ones = ee.Image(ee.Array([1])); 
+//prepend a one - when we take deltas of deltas to get the years it changed everything shifts by a year. 
+//we also lose the first and last breakpoint years because no changes occurred in those years. We need to those to 
+//calculate the magnitude of change for each segment so we need to add ones for those so that they are selected from the 
+//raw data when segment magnitude changes are calculated. 
+yod = (ones.addBands(yod).toArray(0)).toInt() //cast to int because there's a weird floating pt thing going on 
+//add a 1 at the end- because this was based on the deltas we lost the last item in the array but we want it for the mask
+yod = yod.addBands(ones).toArray(0)
+
+//now make a mask
+var yrMask = yod.neq(0)
+
+//get the start values for each segment
+var startVals = ftv.arrayMask(yrMask);
+//get the backwards difference- this will be the magnitude of change between two breakpoints 
+var segChange = backwardsDifference(startVals)
+//calculate the max change- this is the largest change between two vertices over the time series 
+var maxChange = segChange.arrayReduce(ee.Reducer.max(),[0])
+
+//get the index in the array of the end point year when a shift occurred and convert it to an image with a scaler band 
+var pos_of_max = segChange.arrayArgmax()
+                          .arrayProject([0])
+                          .arrayFlatten([['max']])
+//get the breakpoint years, dropping the first which is always the start year                          
+var changeYrs = breakpoints.arraySlice(0,1) 
+//get just the end year of the segment that showed the greatest change in the time series 
+var max_yod = changeYrs.arrayGet(pos_of_max)
+
+// Map.addLayer(changeYrs.arrayGet(pos_of_max),{},'get test')
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////////////////////////////////////////
+//make images from the array images above so we can combine data and visualize 
+var array_to_img = function(array,band){
+  return array.arrayProject([0]) 
+              .arrayFlatten([[band]]) //create a band in the output image 
+}; 
+//create images for the max segment delta and the associated end point year 
+var maxChange_img = array_to_img(maxChange,'max_val'); 
+var maxChangeYrs_img = array_to_img(max_yod,'end_year'); 
+print(maxChangeYrs_img)
+//combine the two images
+maxChange_img = maxChange_img.addBands(maxChangeYrs_img); 
+print(maxChange_img)
+// set visualization dictionaries
+var palette = ['#9400D3', '#4B0082', '#0000FF', '#00FF00', '#FFFF00', '#FF7F00', '#FF0000'];
+var yodVizParams = {
+  min: startYear,
+  max: endYear,
+  palette: palette
+};
+
+var magVizParams = {
+  min: 0,
+  max: 300,
+  palette: palette
+};
+Map.addLayer(maxChange_img.select(['max_val']),magVizParams,'Magnitude of segment change')
+Map.addLayer(maxChange_img.select(['end_year']),yodVizParams,'End year of detection')
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 // check what the outputs look like
@@ -165,27 +238,27 @@ Map.addLayer(difference,{},'diff');
 // print(right)
 // print(diff)
 //check the outputs for a set location 
-var pt_location = ftv.reduceRegion({
-  reducer:ee.Reducer.first(), 
-  geometry:geometry4, 
-  scale:30
-})
-
-var pt_bps = breakpoints.reduceRegion({
-  reducer:ee.Reducer.first(), 
-  geometry:geometry4, 
-  scale:30
-})
-
-// var pt_location = servir_ic.reduceRegion({
+// var pt_location = ftv.reduceRegion({
 //   reducer:ee.Reducer.first(), 
 //   geometry:geometry4, 
 //   scale:30
 // })
 
-// print()
-print(pt_bps,'bps')
-print(pt_location,'ftv')
+// var pt_bps = breakpoints.reduceRegion({
+//   reducer:ee.Reducer.first(), 
+//   geometry:geometry4, 
+//   scale:30
+// })
+
+// // var pt_location = servir_ic.reduceRegion({
+// //   reducer:ee.Reducer.first(), 
+// //   geometry:geometry4, 
+// //   scale:30
+// // })
+
+// // print()
+// print(pt_bps,'bps')
+// print(pt_location,'ftv')
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 //visualize the outputs
 
